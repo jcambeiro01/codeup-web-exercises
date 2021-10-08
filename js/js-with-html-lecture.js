1c9a6e0e00bf15097b8b8806d5fc6b1d55c45311
Jonathan Cambeiro <jonathan.cambeiro01@gmail.com>
2021-10-08 10:49:16 -0500

    completed the js with html lec

---
+++ js/js-with-html-lecture.js
@@ -0,0 +1,14 @@
+''"Use strict";
+var jeff = "Jeff";
+console.log(jeff);
+console.log("Hello I am in my own file now!");
+var returnOfAlert = alert("TGIF Thank Goodness I'm Fred!");
+console.log(returnOfAlert);
+var returnOfConfirm=confirm("Got any plans for this three day weekend?");
+console.log(returnOfConfirm);
+var favoriteMetalBand= prompt("What is your favorite nu-metal band? + !");
+console.log("ha ha you like "+ favoriteMetalBand + " !");
+var leastFavoriteSoda = prompt("What is your least favorite soda?");
+var favoriteSoda = prompt("whats your favorite soda?");
+
+alert("Hello fellow teens, my name is " + leastFavoriteSoda + " " + favoriteSoda + "and i choose hugs not drugs");
